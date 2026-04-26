7c8cb6b1383e724f1cfdb1cefb474525fa532436
Lysenko <lysenkoden@gmail.com>
2026-04-26 07:55:52 +0200

    feat: finalize the project

--- main.py
+++ main.py
@@ -1,11 +1,33 @@
+import random 
+
 def spin_row():
-  pass
+  symbols=['🍒', '🍉', '🍋', '🔔', '⭐']
 
-def print_row():
-  pass
+  # results={}
+  # for symbol in range(3):
+  #   results.append(random.choice(symbols))
+  # return results
 
-def get_payout():
-  pass
+  return [random.choice(symbols) for _ in range(3)]
+
+def print_row(row):
+  print('*************')
+  print(' | '.join(row))
+  print('*************')
+
+def get_payout(row,bet):
+  if row[0]==row[1]==row[2]:
+    if row[0] == '🍒':
+      return bet*3
+    elif row[0] == '🍉':
+      return bet*4
+    elif row[0] == '🍋':
+      return bet*5
+    elif row[0] == '🔔':
+      return bet*10
+    elif row[0] == '⭐':
+      return bet*20
+  return 0
 
 def main():
   balance=100
@@ -34,7 +56,24 @@ def main():
       continue
 
     balance-=bet
+    row= spin_row()
+    print('Spinning...\n')
+    print_row(row)
 
+    payout=get_payout(row,bet)
+
+    if payout>0:
+      print(f"You won ${payout}")
+    else:
+      print("Sorry, you lost this round")
+    balance+=payout
+
+    play_again=input('Do you want to spin again? (Y/N):').upper()
+    if play_again != 'Y':
+      break
+  print('*************************************')
+  print(f"Game over! Your final balance is ${balance}")
+  print('*************************************')
 main()
 if __name__=='main__':
   main()
